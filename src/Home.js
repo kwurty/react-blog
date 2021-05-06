ec03794c5425bdca9efcb86d5f53188a832c301e
Kurtis Soroka <sorok1km@cmich.edu>
2021-05-06 15:47:41 -0500

    continue

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 const Home = () => {
     // let name = 'mario';
@@ -16,28 +16,39 @@ const Home = () => {
     //     setName('luigi');
     //     setAge(32);
     // }
-    const [blogs, setBlogs] = useState([
-        {
-            title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1
-        },
-        {
-            title: 'Ha cha cha', body: 'lorem ipsum...', author: 'mario', id: 2
-        },
-        {
-            title: 'You cannot stop me', body: 'lorem ipsum...', author: 'mario', id: 3
-        },
-        {
-            title: 'Eat at Joes', body: 'lorem ipsum...', author: 'mario', id: 4
-        },
-    ])
+    const [blogs, setBlogs] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    // You cannot use ASYNC for this. Promises work, but for ASYNC to be useable, you need to create a separate function that is async and call it here
+    // useEffect is generally used to 
+    useEffect(() => {
+        setLoading(true);
+        setError(null);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (!res.ok) throw Error('Unable to retrieve data from endpoint')
+                return res.json()
+            })
+            .then((data) => {
+                setLoading(false);
+                setBlogs(data);
+            })
+            .catch((e) => {
+                setLoading(false);
+                setError(e.message);
+            })
+    }, [])
 
     return (
         <div className="home">
+            { error && <div>{error}</div>}
+            { isLoading && <div> Loading ...</div>}
             {/* 
             Passing props to a component is as easy as entering what you want the name to be and what the value should be (in curly braces)
             All declared variables are stored under the props object. Props object must be received as a parameter
             */}
-            <BlogList blogs={blogs} />
+            {blogs && <BlogList blogs={blogs} />}
 
             {/* <p> {name} is {age} years old</p> */}
             {/* <button onClick={handleClick}> Click me </button> */}
